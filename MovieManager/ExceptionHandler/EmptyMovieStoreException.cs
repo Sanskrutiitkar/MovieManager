4a6d5549cb2af3a63047d889a45807ffe074f70b
Sanskruti Itkar <139566431+Sanskrutiitkar@users.noreply.github.com>
2024-07-30 20:28:24 +0530

    Add files via upload

---
+++ MovieManager/ExceptionHandler/EmptyMovieStoreException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieManager.ExceptionHandler
+{
+    internal class EmptyMovieStoreException : Exception
+    {
+        public EmptyMovieStoreException(string msg) : base(msg)
+        {
+        }
+    }
+}
